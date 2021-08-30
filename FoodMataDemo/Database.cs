be29dac8b638f200edf3b35aec2d639f185d12af
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2021-08-30 11:23:11 +0100

    Added local files to the git repo

---
+++ FoodMataDemo/Database.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FoodMataDemo
+{
+    class Database
+    {
+        public SQLiteConnection myConn;
+       
+        string dataBaseName = "Foodmata.sqlite";
+        public Database()
+        {
+            myConn = new SQLiteConnection("Data Source = " + dataBaseName);
+            if (!File.Exists("./"+ dataBaseName))
+            {
+                SQLiteConnection.CreateFile(dataBaseName);
+            }
+        }
+        public void openConnection()
+        {
+            if (myConn.State != System.Data.ConnectionState.Open)
+            {
+                myConn.Open();
+            }
+        }
+        public void closeConnection()
+        {
+            if (myConn.State != System.Data.ConnectionState.Closed)
+            {
+                myConn.Close();
+            }
+        }
+       
+        public void InsertData(string name, string location, string phoneNo, string accountName, long accountNo, string bank, string website )
+        {
+            string query = "INSERT INTO Restaurants ('Name', 'Location', 'Phone No', " +
+                "'Account Name', 'Account No', 'Bank', 'Website') VALUES (@N, @L, @P, @AN, @AC, @B, @W);";
+            SQLiteCommand sQLiteCommand = new SQLiteCommand(query, myConn);
+            openConnection();
+
+            sQLiteCommand.Parameters.AddWithValue("@N",name);
+            sQLiteCommand.Parameters.AddWithValue("@L",location);
+            sQLiteCommand.Parameters.AddWithValue("@P",phoneNo);
+            sQLiteCommand.Parameters.AddWithValue("@AN",accountName);
+            sQLiteCommand.Parameters.AddWithValue("@AC",accountNo);
+            sQLiteCommand.Parameters.AddWithValue("@B",bank);
+            sQLiteCommand.Parameters.AddWithValue("@W",website);
+
+            sQLiteCommand.ExecuteNonQuery();
+            closeConnection();
+        }
+        public void UpdateData(string name, string location, string phoneNo, string accountName, long accountNo, string bank, string website)
+        {
+            string query = "UPDATE Restaurants SET Name=@N, Location=@L, [Phone No]=@P, " +
+                "[Account Name]=@AN, [Account No]=@AC, Bank=@B, Website=@W WHERE [Account No]=@AC;";
+
+            SQLiteCommand sQLiteCommand = new SQLiteCommand(query, myConn);
+            openConnection();
+            
+            sQLiteCommand.Parameters.AddWithValue("@N", name);
+            sQLiteCommand.Parameters.AddWithValue("@L", location);
+            sQLiteCommand.Parameters.AddWithValue("@P", phoneNo);
+            sQLiteCommand.Parameters.AddWithValue("@AN", accountName);
+            sQLiteCommand.Parameters.AddWithValue("@AC", accountNo);
+            sQLiteCommand.Parameters.AddWithValue("@B", bank);
+            sQLiteCommand.Parameters.AddWithValue("@W", website);
+            
+            sQLiteCommand.ExecuteNonQuery();
+            closeConnection();
+        }
+        public void DeleteData(string name, string location, string phoneNo, string accountName, long accountNo, string bank, string website)
+        {
+            string query = "DELETE FROM Restaurants WHERE [Account No]=@AC ;";
+
+            SQLiteCommand sQLiteCommand = new SQLiteCommand(query, myConn);
+            openConnection();
+
+            sQLiteCommand.Parameters.AddWithValue("@N", name);
+            sQLiteCommand.Parameters.AddWithValue("@L", location);
+            sQLiteCommand.Parameters.AddWithValue("@P", phoneNo);
+            sQLiteCommand.Parameters.AddWithValue("@AN", accountName);
+            sQLiteCommand.Parameters.AddWithValue("@AC", accountNo);
+            sQLiteCommand.Parameters.AddWithValue("@B", bank);
+            sQLiteCommand.Parameters.AddWithValue("@W", website);
+            
+            sQLiteCommand.ExecuteNonQuery();
+            closeConnection();
+        }
+    }
+}
---
+++ FoodMataDemo/FoodMata.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FoodMataDemo
+{
+    public partial class FoodMata : Form
+    {
+        Database databaseObject = new Database();
+        public FoodMata()
+        {
+            InitializeComponent();
+            
+        }
+        string User_Name, User_Location, Phone_Number, Account_Name, Bank_Name, Website;
+
+        
+
+        long Account_Number;
+
+        private void FoodMata_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void ClearAll()
+        {
+            txtName.Clear();
+            txtLocation.Clear();
+            txtPhoneNumber.Clear();
+            txtAccountName.Clear();
+            txtAccountNumber.Clear();
+            txtBankName.Clear();
+            txtWebsite.Clear();
+        }
+        private void btnsubmit_Click(object sender, EventArgs e)
+        {
+            DeclareAssign();
+            if (User_Name == "" || User_Location == "" || Phone_Number == "" || Account_Name == "" || Bank_Name == "" || Website == "" || txtAccountNumber.Text == "")
+            {
+                MessageBox.Show("Field(s) cannot be empty.\n Please fill all fields");
+            }
+            else
+            {
+                //databaseObject.CreateTable();
+                databaseObject.InsertData(User_Name, User_Location, Phone_Number, Account_Name, Account_Number, Bank_Name, Website);
+                MessageBox.Show("Your Data has been saved.\nThank you");
+                ClearAll();
+            }
+
+        }
+        public void DeclareAssign()
+        {
+            User_Name = txtName.Text;
+            User_Location = txtLocation.Text;
+            Phone_Number = txtPhoneNumber.Text;
+            Account_Name = txtAccountName.Text;
+            Bank_Name = txtBankName.Text;
+            Website = txtWebsite.Text;
+            try
+            {
+                Account_Number = long.Parse(txtAccountNumber.Text);
+            }
+            catch (Exception ex)
+            {
+                txtAccountNumber.Clear();
+                MessageBox.Show("Account Number must be integer\n" + ex.Message);
+            }
+        }
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("To Update an entry, your account number must be accurate\nYour data won't be added or edited if account number doesn't exist\n click Okay to continue, Cancel to edit account number", "Information", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if(dr == DialogResult.OK)
+            { 
+                DeclareAssign();
+                if (User_Name == "" || User_Location == "" || Phone_Number == "" || Account_Name == "" || Bank_Name == "" || Website == "" || txtAccountNumber.Text == "")
+                {
+                    MessageBox.Show("Field(s) cannot be empty.\n Please fill all fields");
+                }
+                else
+                {
+                    //databaseObject.UpdateTable();
+                    databaseObject.UpdateData(User_Name, User_Location, Phone_Number, Account_Name, Account_Number, Bank_Name, Website);
+                    MessageBox.Show("Your Data has been Updated.\nThank you");
+                    ClearAll();
+                }
+            }
+        }
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("To Delete an entry, your account number must be accurate\nYour data won't be added or edited if account number doesn't exist\n click Okay to continue, Cancel to edit account number", "Information", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (dr == DialogResult.OK)
+            {
+                DeclareAssign();
+                if (User_Name == "" || User_Location == "" || Phone_Number == "" || Account_Name == "" || Bank_Name == "" || Website == "" || txtAccountNumber.Text == "")
+                {
+                    MessageBox.Show("Field(s) cannot be empty.\n Please fill all fields");
+                }
+                else
+                {
+                    //databaseObject.UpdateTable();
+                    databaseObject.DeleteData(User_Name, User_Location, Phone_Number, Account_Name, Account_Number, Bank_Name, Website);
+                    MessageBox.Show("Your Entry has been Deleted.\nThank you");
+                    ClearAll();
+                }
+            }
+        }
+    }
+}
